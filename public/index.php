3f67b1bcc135c5e2f1affa46d857dc75e0acffb4
IBobbyBrownI <shawnbrownmail@gmail.com>
2026-05-26 23:24:22 +0300

    added report from 05.26.2026

--- public/index.php
+++ public/index.php
@@ -2,7 +2,7 @@
 
 require_once __DIR__ . "/../vendor/autoload.php";
 
-Use Symfony\Component\Dotenv\Dotenv;
+use Symfony\Component\Dotenv\Dotenv;
 
 $dotenv = new Dotenv();
 $dotenv->load(__DIR__ . "/../.env");
